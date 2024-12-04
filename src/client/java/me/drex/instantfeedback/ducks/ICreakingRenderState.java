f34a09b7230c55b2796ff4eb530e0e2f45e3eee5
Drex <nicknamedrex@gmail.com>
2024-12-04 11:48:07 +0100

    Update to 1.21.4
    relates to #1

--- src/client/java/me/drex/instantfeedback/ducks/ICreakingRenderState.java
+++
@@ -1,6 +0,0 @@
-package me.drex.instantfeedback.ducks;
-
-public interface ICreakingRenderState {
-    void instantfeedback$setTicksSinceLastMove(long ticksSinceLastMove);
-    long instantfeedback$getTicksSinceLastMove();
-}
