0c49a85056b64f89e094124d268734d774ed617b
Drex <nicknamedrex@gmail.com>
2024-12-13 19:53:15 +0100

    Improve various tags
    Add Pale Roses to flower item tag
    
    Add Pale Roses to Small Flower block tag
    
    Pale Bush parity tags with Dead Bush
    
    fixes #14

--- src/main/java/me/drex/instantfeedback/datagen/ModBlockTagProvider.java
+++ src/main/java/me/drex/instantfeedback/datagen/ModBlockTagProvider.java
@@ -2,11 +2,13 @@ package me.drex.instantfeedback.datagen;
 
 import me.drex.instantfeedback.InstantFeedback;
 import me.drex.instantfeedback.block.ModBlocks;
+import me.drex.instantfeedback.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,13 +25,22 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
         this.getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_AXE)
             .add(ModBlocks.PALE_PUMPKIN)
-            .add(ModBlocks.CARVED_PALE_PUMPKIN);
+            .add(ModBlocks.CARVED_PALE_PUMPKIN)
+            .add(ModBlocks.PALE_BUSH, ModBlocks.TALL_PALE_BUSH);
+
         this.getOrCreateTagBuilder(BlockTags.SWORD_EFFICIENT)
             .add(ModBlocks.PALE_PUMPKIN)
-            .add(ModBlocks.CARVED_PALE_PUMPKIN);
+            .add(ModBlocks.CARVED_PALE_PUMPKIN)
+            .add(ModBlocks.PALE_BUSH, ModBlocks.TALL_PALE_BUSH);
 
         this.getOrCreateTagBuilder(BlockTags.BEE_ATTRACTIVE)
             .add(ModBlocks.PALE_ROSE);
+
+        this.getOrCreateTagBuilder(BlockTags.SMALL_FLOWERS)
+                .add(ModBlocks.PALE_ROSE);
+
+        this.getOrCreateTagBuilder(BlockTags.REPLACEABLE_BY_TREES)
+                .add(ModBlocks.PALE_BUSH, ModBlocks.TALL_PALE_BUSH);
 
         FabricTagBuilder builder = this.getOrCreateTagBuilder(BlockTags.REPLACEABLE);
         wrapperLookup.lookupOrThrow(Registries.BLOCK)
---
+++ src/main/java/me/drex/instantfeedback/datagen/ModItemTagProvider.java
@@ -0,0 +1,22 @@
+package me.drex.instantfeedback.datagen;
+
+import me.drex.instantfeedback.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.ItemTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider wrapperLookup) {
+        this.getOrCreateTagBuilder(ItemTags.SMALL_FLOWERS)
+            .add(ModItems.PALE_ROSE);
+    }
+}
